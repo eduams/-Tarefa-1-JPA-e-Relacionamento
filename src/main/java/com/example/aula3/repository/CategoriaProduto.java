b1f5f404bbcce88490cdf6f679b2feb847bd0ab9
Eduardo Alves <eduams@eduams.eduams>
2023-09-28 20:21:19 -0300

    first commit

---
+++ src/main/java/com/example/aula3/repository/CategoriaProduto.java
@@ -0,0 +1,48 @@
+package com.example.aula3.repository;
+
+import java.util.List; 
+
+import com.example.aula3.models.Produto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="categoria_produto")
+public class CategoriaProduto {
+@OneToMany(mappedBy = "categoriaProduto")
+private List<Produto> produtos;
+public List<Produto> getProdutos() {
+    return produtos;
+    }
+    public void setProdutos(List<Produto> produtos) {
+    this. produtos = produtos;
+    }
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+private String nome;
+public CategoriaProduto() {
+}
+public CategoriaProduto(int id, String nome) {
+this.id = id;
+this.nome = nome;
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+}
---
+++ src/main/java/com/example/aula3/repository/CategoriaProdutoRepository.java
@@ -0,0 +1,24 @@
+package com.example.aula3.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class CategoriaProdutoRepository {
+@Autowired
+private EntityManager entityManager;
+@Transactional
+public CategoriaProduto inserir(CategoriaProduto categoriaProduto) {
+entityManager.persist(categoriaProduto);
+return categoriaProduto;
+}
+public List< CategoriaProduto > obterTodos() {
+return entityManager.createQuery("from categoriaProduto", CategoriaProduto.class).getResultList();
+}
+}
